001f36ff614e6a975800bcfd22d6ce792aa54afe
angelbotia <angelbotia.dev@gmail.com>
2025-08-02 23:53:51 +0200

    feat: middleware cookies

--- dan-bookings/src/middleware.js
+++ dan-bookings/src/middleware.js
@@ -1,3 +1,12 @@
 export { default } from "next-auth/middleware"
+import { NextResponse } from 'next/server';
+import { LANG_LS } from "./app/(GUI)/application/application.constant";
 
-export const config = { matcher: ["/booking"] }
+export const config = { matcher: ['/((?!api|_next|favicon.ico).*)',"/booking"] }
+
+export function middleware(request) {
+  const lang = request.cookies.get(LANG_LS)?.value || DEFAULT_LNG_APP;
+  const response = NextResponse.next();
+  response.cookies.set(LANG_LS, lang, { path: '/' });
+  return response;
+}
